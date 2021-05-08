e0c4d2d60d2229605be06121346608f8e9068c82
Łukasz <lukasz.kordon@outlook.com>
2021-05-08 04:03:40 +0200

    describe new python modul

---
+++ 6.Python_stdlib/functools_reduce.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 02:05:35 2021
+
+@author: lukas
+"""
+import functools
+  
+myresult = [(("1"), ("2"), ("3"), ("4"))]
+print(myresult)
+# reduce(function, sequence)
+myresult = functools.reduce(lambda sub, ele: sub  + ele, myresult)
+print(myresult)
+
+# lambda remainder
+myresult = lambda sub, ele: sub + ele
+print(myresult(2,4))
+
