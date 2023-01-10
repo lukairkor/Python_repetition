2bfe3a14ee2f04e3c8e5259493611cf87e9111d5
lukair <lukasz.kordon@outlook.com>
2023-01-10 21:11:35 +0100

    starting implementing linkedlist

--- 0.Python_Data_Structures/User_defined/linked_list.py
+++ 0.Python_Data_Structures/User_defined/linked_list.py
@@ -8,15 +8,59 @@ Created on Fri Apr  1 22:20:09 2022
 - linear data struct
 @author: lukas
 """
+# data next
+# |2|  | | --> | |  | | --> Null
+#  ^ head pointer
 
-from collections import deque
+class Node:
+    """ """
+    def __init__(self, data):
+        # attributes
+        self.data = data # hold a value nof node
+        self.next = None # hold reference to next node
 
-dq = deque([1, 2, 3, 4, 5, 6])
 
-print(dq)
+class LinkedList:
+    """ """
+    def __init__(self):
+        self.head = None # point to first node in the list
 
-dq.appendleft(0)
-print(dq)
 
-dq.popleft()
-print(dq)
+    def add_front(self, data):
+        """ """
+        node = Node(data) # node object
+        node.next = self.head
+        self.head = node
+
+
+    def add_back(self, data):
+        """ """
+        new_node = Node(data) # all data into ew_node
+        
+        if not self.head: # check if list is empty
+            self.head = new_node 
+            return
+            
+        
+        None
+
+
+    def show_all(self):
+        """ """
+        actu_nod = self.head
+
+        while actu_nod: # is not None
+            print(actu_nod.data)
+            actu_nod = actu_nod.next
+
+
+
+lis = LinkedList()
+
+lis.add_front(1)
+lis.add_front(2)
+lis.add_front(3)
+lis.show_all()
+
+# prints 3, 2, 1
+
